4498dc5d68f76c29aad481246d09b4502c8be4d9
damianmac <damian@damianm.com>
2019-05-07 14:29:40 +1000

    Init

---
+++ src/SqlScripter/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SqlScripter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
