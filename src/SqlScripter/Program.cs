32ced2e8082e9b3a14e1ea3f9d0a99f835e22844
damianmac <damian@damianm.com>
2019-05-08 11:03:54 +1000

    removed crap

--- src/SqlScripter/Program.cs
+++ src/SqlScripter/Program.cs
@@ -24,20 +24,6 @@ namespace SqlScripter
             });
             return app.Run(args);
 
-            // Console.WriteLine("Hello World!");
-
-            // var tasks = new []{typeof(ScriptTasks.ScriptAllTables), typeof(ScriptTasks.ScriptStoredProcedures), typeof(ScriptTasks.ScriptUserDefinedFunctions)};
-
-            // using(var conn = new DatabaseConnection("localhost", "db", "user", "password!"))
-            // {
-            //     var task = (SqlScriptTask)Activator.CreateInstance(tasks.Last());
-            //     task.DatabaseConnection = conn;
-            //     task.OutputDirectory = "./";
-            //     task.Run();
-
-            // }
-
-            // Console.WriteLine("Done");
         }
     }
 }
